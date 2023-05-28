bc88dbe5f827b571ba2d47af3b960f6a1218e1dc
Maryam Moshootfar <maryammoshtoofar@gmail.com>
2023-05-28 11:19:49 +0330

    api:interact

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,10 +9,9 @@ const { API_URL, PRIVATE_KEY } = process.env;
 
 module.exports = {
   solidity: "0.8.18",
-  defaultNetwork: "sepolia",
+  defaultNetwork: "goerli",
   networks: {
-    hardhat: {},
-    sepolia: {
+    goerli: {
       url: API_URL,
       accounts: [`0x${PRIVATE_KEY}`],
     },
